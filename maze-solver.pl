46158c5094d1ea3cde9911c9ed41faa39ea21711
Andrew Ho <andrewyc.ho@gmail.com>
2015-04-12 21:45:08 +0100

    redesigned possMoves/2 predicate
    using a list filter this time to include only the moves to free space.
    Not working yet

--- maze-solver.pl
+++ maze-solver.pl
@@ -1,13 +1,13 @@
 
 /* ******************************************************************************** */
 /*                                                  						   		*/
-/*   Your task is to write a predicate:             								 */
+/*   Your task is to write a predicate:             								*/
 /*                                                  						   		*/
 /*      solve(From,To,Path)                         						   		*/
 /*                                                  						   		*/
 /*   which, given locations From and To, finds a Path going from From to To.   		*/
 /*   From and To are given as two element lists, and Path should be a list   		*/
-/*   of two-element lists. The first element of Path should be From, and the   	  */
+/*   of two-element lists. The first element of Path should be From, and the   	    */
 /*   last element should be To. Moves can be made horizontally or vertically,  		*/
 /*   but not diagonally.													   		*/
 /*                                                  						   		*/
@@ -16,7 +16,7 @@
 /*   solve([3,2], [2,6], [[3,2], [3,3], [2,3], [1,3], [1,4], [1,5], [1,6], [2,6]]). */
 /*                                                  						   		*/
 /*   should print the solution as a list, and also as a text drawing.   	 	    */
-/*   You do not have to find the shortest path.          				   		*/
+/*   You do not have to find the shortest path.          				   		    */
 /*                                                  						   		*/
 /*   Author: Andrew Ho							                               		*/
 /*   Date: 12 April 2015							                           		*/
@@ -33,6 +33,34 @@ freeSpace([X,Y]) :- not(barrier(X,Y)),
 					
 % Returns a list with containing the co-ordinates of all possible moves from a cell. 
 
-possMoves([X,Y],L) :- Xup is X-1,freeSpace([Xup,Y]),append([Xup,Y],L)
+moveUp([X,Y],[Xup,Y]) :- Xup is X-1.
+moveDown([X,Y],[Xdown,Y]) :- Xdown is X+1.
+moveLeft([X,Y],[X,Yleft]) :- Yleft is Y-1.
+moveRight([X,Y],[X,Yright]) :- Yright is Y+1.
+
+filterMoves(A,In,Out) :- exclude(not(freeSpace(A)),In,Out).
+
+possMoves([X,Y],M) :- moveUp([X,Y],Up),
+					  moveDown([X,Y],Down),
+					  moveLeft([X,Y],Left),
+					  moveRight([X,Y],Right),					  					  
+					  filterMoves(A,[Up,Down,Left,Right],M).
+
 
 % solve([FromX,FromY],[ToX,ToY],Path) :- 
+
+
+
+
+/* ******************************************************************************** */
+/*   Rough Working Notes                               						   		*/
+/* ******************************************************************************** */
+
+%possMoves([X,Y],M) :- checkMoveUp([X,Y],M).
+%possMoves([X,Y],M) :- checkMoveDown([X,Y],M).
+
+%checkMoveUp([X,Y],[Xup,Y]) :- Xup is X-1,
+%					 	 	  freeSpace([Xup,Y]).
+
+%checkMoveDown([X,Y],[Xdown,Y]) :- Xdown is X+1,
+%   				 	 		  freeSpace([Xdown,Y]).
