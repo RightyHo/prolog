7a66388bcf35571ccab287d4877d51279206aa58
Andrew Ho <andrewyc.ho@gmail.com>
2015-04-13 20:01:00 +0100

    wrote possibleStep/2 predicate which
    checks whether a step from point A to point B is possible.  Returns
    true if possible and false otherwise.  Also started writing solve/3 and
    solve/4 predicates

--- maze-solver.pl
+++ maze-solver.pl
@@ -1,3 +1,4 @@
+% working_directory(_,'Users/andrewho/Documents/Programming/MSc/Paradigms/Prolog/prolog').
 
 /* ******************************************************************************** */
 /*                                                  						   		*/
@@ -29,26 +30,43 @@
 freeSpace([X,Y]) :- not(barrier(X,Y)),
 	 				mazeSize(BoardX,BoardY),
 					X=<BoardX, 
-					Y=<BoardY.					
+					Y=<BoardY.								
 					
-% Returns a list with containing the co-ordinates of all possible moves from a cell. 
+% checks whether a step from point A to point B is possible.  Returns true if possible and false otherwise.
+					
+possibleStep([Xa,Ya],[Xb,Yb]) :- Xb is Xa+1,
+					  	 Yb==Ya,
+					  	 freeSpace([Xa,Ya]),
+					  	 freeSpace([Xb,Yb]).
 
-moveUp([X,Y],[Xup,Y]) :- Xup is X-1.
-moveDown([X,Y],[Xdown,Y]) :- Xdown is X+1.
-moveLeft([X,Y],[X,Yleft]) :- Yleft is Y-1.
-moveRight([X,Y],[X,Yright]) :- Yright is Y+1.
-
-filterMoves(A,In,Out) :- exclude(not(freeSpace(A)),In,Out).
-
-possMoves([X,Y],M) :- moveUp([X,Y],Up),
-					  moveDown([X,Y],Down),
-					  moveLeft([X,Y],Left),
-					  moveRight([X,Y],Right),					  					  
-					  filterMoves(A,[Up,Down,Left,Right],M).
+possibleStep([Xa,Ya],[Xb,Yb]) :- Xb is Xa-1,
+					  	 Yb==Ya,
+					  	 freeSpace([Xa,Ya]),
+					  	 freeSpace([Xb,Yb]).
 
 
-% solve([FromX,FromY],[ToX,ToY],Path) :- 
+possibleStep([Xa,Ya],[Xb,Yb]) :- Xb==Xa,
+					  	 Yb is Ya+1,
+					  	 freeSpace([Xa,Ya]),
+					  	 freeSpace([Xb,Yb]).
 
+possibleStep([Xa,Ya],[Xb,Yb]) :- Xb==Xa,
+					  	 Yb is Ya-1,
+					  	 freeSpace([Xa,Ya]),
+					  	 freeSpace([Xb,Yb]).
+						 	
+% finds a Path going from From to To avoiding barriers and staying within the board limits
+
+solve([FromX,FromY],[ToX,ToY],Path) :-  solve([FromX,FromY],[ToX,ToY],[],Path).
+							
+solve([X,Y],[X,Y],Route,[Route]).
+
+% solve([FromX,FromY],[ToX,ToY],AccRoute,Path) :- possibleStep([FromX,FromY],[ToX,ToY]).
+
+solve([FromX,FromY],[ToX,ToY],AccRoute,Path) :- possibleStep([FromX,FromY],[ViaX,ViaY]),
+					  						 	not(member([ViaX,ViaY],AccRoute)),
+					  					   	 	solve([ViaX,ViaY],[ToX,ToY],[[ViaX,ViaY]|AccRoute],Path).
+	
 
 
 
@@ -56,11 +74,17 @@ possMoves([X,Y],M) :- moveUp([X,Y],Up),
 /*   Rough Working Notes                               						   		*/
 /* ******************************************************************************** */
 
-%possMoves([X,Y],M) :- checkMoveUp([X,Y],M).
-%possMoves([X,Y],M) :- checkMoveDown([X,Y],M).
+% Returns a list with containing the co-ordinates of all possible moves from a cell. 
 
-%checkMoveUp([X,Y],[Xup,Y]) :- Xup is X-1,
-%					 	 	  freeSpace([Xup,Y]).
+% moveUp([X,Y],[Xup,Y]) :- Xup is X-1.
+% moveDown([X,Y],[Xdown,Y]) :- Xdown is X+1.
+% moveLeft([X,Y],[X,Yleft]) :- Yleft is Y-1.
+% moveRight([X,Y],[X,Yright]) :- Yright is Y+1.
 
-%checkMoveDown([X,Y],[Xdown,Y]) :- Xdown is X+1,
-%   				 	 		  freeSpace([Xdown,Y]).
+% filterMoves(A,In,Out) :- exclude(not(freeSpace(A)),In,Out).
+
+% possMoves([X,Y],M) :- moveUp([X,Y],Up),
+%					  moveDown([X,Y],Down),
+%					  moveLeft([X,Y],Left),
+%					  moveRight([X,Y],Right),					  					  
+%					  filterMoves(A,[Up,Down,Left,Right],M).
