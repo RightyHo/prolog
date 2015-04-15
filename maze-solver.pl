8ea9a69ca831af80499e3abebf9b4cb9b03b14c2
Andrew Ho <andrewyc.ho@gmail.com>
2015-04-15 15:54:29 +0100

    added code for printGrid/1 predicate
    to print the grid.  needs work

--- maze-solver.pl
+++ maze-solver.pl
@@ -55,7 +55,8 @@ possibleStep([X,Y],[X,Ynew]) :- Ynew is Y-1,
 
 solve([FromX,FromY],[ToX,ToY],ThePath) :-  solve([FromX,FromY],[ToX,ToY],[[FromX,FromY]|[]],Path),
 										   reverse(Path,[],ThePath),
-										   printPath(ThePath).
+										   printPath(ThePath),
+										   printGrid([ThePath|[]]).
 							
 solve([X,Y],[X,Y],Route,Route).		
 
@@ -76,7 +77,49 @@ printPath([]).
 printPath([H|T]) :- write(H), nl,
 					printPath(T).
 
+% code for printing out the grid
 
+printGrid([]).
+printGrid(L) :- nl,
+   divider,
+   printBlock(L,L1),
+   divider,
+   printBlock(L1,L2),
+   divider,
+   printBlock(L2,_),
+   divider.
+
+printBlock(L,L3) :- 
+   printRow(L,L1), nl,
+   blankLine, 
+   printRow(L1,L2), nl,
+   blankLine, 
+   printRow(L2,L3), nl.
+ 
+printRow(L,L3) :-
+   write('+ '),
+   printTriplet(L,L1), write(' | '),
+   printTriplet(L1,L2), write(' | '),
+   printTriplet(L2,L3),
+   write(' +').
+
+
+blankLine :- 
+   write('+         |         |         +'), nl.
+
+
+% blankLine.
+
+divider :-
+   write('+---------+---------+---------+'), nl.
+
+printTriplet(L,L3) :-
+   printElement(L,L1), write('  '),
+   printElement(L1,L2), write('  '),
+   printElement(L2,L3).
+
+printElement([X|L],L) :- var(X), !, write('.').
+printElement([X|L],L) :- write(X).
 
 
 
